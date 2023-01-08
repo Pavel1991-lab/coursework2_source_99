55f26f255e38afb091db86e6fe9fbfa08535d120
Павел <sotnikov.pavel.91@mail.ru>
2023-01-08 18:48:19 +0300

    Немного курсовой

---
+++ data/utils.py
@@ -0,0 +1,42 @@
+import json
+
+def get_posts_all(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+def get_posts_by_user(user_name):
+    for post in get_posts_all('posts.json'):
+        if post['poster_name'] == user_name:
+            return post["content"]
+        if post['poster_name'] == user_name and len(post["content"]) == 0:
+            return []
+    return f'Vallue Error'
+
+def get_comments_by_post_id(post_id):
+    b = []
+    d = []
+    for coment in get_posts_all('comments.json'):
+       if coment['post_id'] == post_id:
+           b.append(coment["comment"])
+       d.append(coment["post_id"])
+       if post_id not in d:
+           return f'Vallue Error'
+    return b
+
+def search_for_posts(querry):
+    a = []
+    for i in get_posts_all('posts.json'):
+        if querry.lower() in i["content"].lower():
+            a.append(i["content"])
+    return a
+
+def get_post_by_pk(pk):
+    for post in get_posts_all('posts.json'):
+        if post['pk'] == pk:
+            a = post["content"]
+    return a
+
+
+
+
